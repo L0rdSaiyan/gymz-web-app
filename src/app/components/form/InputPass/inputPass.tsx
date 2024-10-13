a4a576315c8cc1e46f723a39b3fbf77bc9e1e390
Victor Sales <thehoodgamer20@gmail.com>
2024-10-13 17:17:09 -0300

    adicionado função de verificação de senhas

--- src/app/components/form/InputPass/inputPass.tsx
+++ src/app/components/form/InputPass/inputPass.tsx
@@ -1,6 +1,11 @@
-export default function InputPass() {
+interface PropsTypes 
+{
+  changeEvent ?: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function InputPass({changeEvent} : PropsTypes) {
     return (
-      <input type="password" placeholder="Insira sua senha" className=" w-full  md:w-1/2 lg:w-1/3 px-4 py-2 border border-gray-300 rounded-lg text-gray-700 focus:outline-none focus:border-blue-500" /> 
+      <input type="password" placeholder="Insira sua senha" onChange={changeEvent} className=" w-full  md:w-1/2 lg:w-1/3 px-4 py-2 border border-gray-300 rounded-lg text-gray-700 focus:outline-none focus:border-blue-500" /> 
     );
   }
   
--- src/app/singup/page.tsx
+++ src/app/singup/page.tsx
@@ -8,11 +8,29 @@ import { useState } from "react";
 export default function Singup() {
 
   const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPass, setConfirmPass] = useState<string>();
 
   const handleUserNameChange = (event : React.ChangeEvent<HTMLInputElement>) =>
   {
     setUsername(event.target.value)
     console.log(username)
+  }
+
+  const handlePassChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+  {
+    setPassword(event?.target.value)
+    console.log(password)
+  }
+
+  const handleConfirmPassChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+  {
+    setConfirmPass(event.target.value)
+  }
+
+  const handleConfirmPass = () =>
+  {
+
   }
 
   return (
@@ -21,8 +39,11 @@ export default function Singup() {
       <form className="flex flex-col space-y-5 w-[50%] h-4/5 items-center justify-center shadow-md rounded-lg backdrop-blur-md">
         <h1 className="bottom-[100px] text-white text-[2.0em] font-">Crie sua conta </h1>
         <InputText text="Insira seu nome de usuário" eventChange={handleUserNameChange}/>
-        <InputPass />
-        <InputPass />
+        <InputPass changeEvent={handlePassChange} />
+        <InputPass changeEvent={handleConfirmPassChange} />
+        {
+          confirmPass !== password && <h1 className="text-red-500">As senhas não conferem!</h1>
+        }
         <SubmitBtn />
       </form>
     </div>
