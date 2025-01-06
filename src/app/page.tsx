87cde11ea6ea619f247da2a8928f99315e5e58e1
Victor <joaovictors193203@gmail.com>
2025-01-05 23:13:44 -0300

    [fix] 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-
-import React, { ReactNode } from 'react';
+import React from 'react';
 import Header from './components/Header/header';
 import './page.css';
 
-export default function Page({ children } : {children : ReactNode}) {
+export default function Page() {
   return (
-    <div className="w-full h-screen bg-red-700">
-      {children}
+    <div className="w-full h-screen">
+      <h1>Bem-vindo à página principal</h1>
     </div>
   );
 }
